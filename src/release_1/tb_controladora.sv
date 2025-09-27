aecbf89b93d5939b64ce40002148b37bb7ed05e8
Jefferson <jeffersonamorim258@gmail.com>
2025-09-27 13:40:13 -0300

    :sparkles: feat: v1.0 - first realease

--- src/release_1/tb_controladora.sv
+++ src/release_1/tb_controladora.sv
@@ -13,11 +13,18 @@ module tb_controladora;
     // Parâmetro de clock
     parameter int METADE_PERIODO = 1; // 1 ns -> periodo total 2 ns
 
+    // Alguns parâmetros
+    parameter int PULSOS_TROCAR_MODO = 5300;
+    parameter int INTERVALO_MIN = 4770;
+    parameter int INTERVALO_MAX = 5830;
+    parameter int QNT_ALEATORIOS = 10;
+    parameter int SEMENTE = 1111;
+
     // Clock
     always #METADE_PERIODO clk = ~clk;
 
     // Instância do DUT
-    controladora #(.SWITCH_MODE_MIN_T(5300)) 
+    controladora #(.SWITCH_MODE_MIN_T(PULSOS_TROCAR_MODO)) 
     dut (
         .clk(clk),
         .rst(rst),
@@ -26,6 +33,22 @@ module tb_controladora;
         .led(led),
         .saida(saida)
     );
+
+    class GeradorAleatorio;
+
+        // randc garante que cada valor na faixa será sorteado uma vez antes de repetir
+        randc bit [12:0] num; // bit [12:0] pode representar valores de 0 a 8191
+
+        int min, max;
+
+        function new(input int min, input int max); // Define o construtor
+            this.min = min;
+            this.max = max;
+        endfunction
+
+        // Define a faixa de valores válidos
+        constraint range_c { num inside {[min:max]}; }
+    endclass
 
     // Dump para waveform
     initial begin
@@ -40,34 +63,55 @@ module tb_controladora;
         end
     endtask
 
-    task automatic teste_alternar_modos(input int qnt_pulsos, 
-                                        input int num_teste, 
-                                        input logic led_esperado);
+    task automatic teste_alternar_modos(input int qnt_pulsos);
         logic resultado;
+        logic led_esperado = qnt_pulsos > PULSOS_TROCAR_MODO;
 
-        resetar();
-        // $display("\n== Teste Segurar push_button por %0d ciclos (%0t ns) ==",
-        //         qnt_pulsos, qnt_pulsos * (2 * METADE_PERIODO));
-        push_button = 1;
+        static int num_teste = 1;
+
+        resetar(); // Realiza o reset no sistema
+
+        push_button = 1; // Pressiona o botão
         repeat(qnt_pulsos) @(posedge clk);
-        push_button = 0;
+        push_button = 0; // Solta o botão
 
         #10;
-        resultado = (led == led_esperado);
-        $display("[%0t] Teste %0d: %s", $time, num_teste,
-                resultado ? "PASSOU!" : "FALHOU!");
+
+        if (led === led_esperado) begin
+            $display("[%0t] Teste %0d: PASSOU! Pulsos: %0d | LED: %b",
+            $time, num_teste, qnt_pulsos, led);
+        end
+        else begin
+            $display("[%0t] Teste %0d: FALHOU! Pulsos: %0d | LED: %b -> ESPERADO: %b",
+            $time, num_teste, qnt_pulsos, led, led_esperado);
+        end
+
+        num_teste++;
         #5;
     endtask
 
+    GeradorAleatorio gen;
+    
+    // Realizando os testes
     initial begin
-        // Teste 1: Pressionar o botão por 5300 pulsos -> Manter modo (led = 0)
-        teste_alternar_modos(5300, 1, 0);
+        // Teste: Pressionar o botão por 5300 pulsos
+        teste_alternar_modos(5300);
 
-        // Teste 2: Pressionar o botão por 5301 pulsos -> Alternar modo (led = 1)
-        teste_alternar_modos(5301, 2, 1);
+        // Teste: Pressionar o botão por 5301 pulsos
+        teste_alternar_modos(5301);
         
-        // Teste 3: Pressionar o botão por 5305 pulsos -> Alternar modo (led = 1)
-        teste_alternar_modos(5305, 3, 1);
+        // Teste: Pressionar o botão por 5305 pulsos
+        teste_alternar_modos(5305);
+
+        // Teste: Pressionar o botão 10 vezes entre 4770 e 5830 pulsos aleatoriamente
+        $srandom(SEMENTE);
+
+        gen = new(INTERVALO_MIN, INTERVALO_MAX);
+
+        repeat(QNT_ALEATORIOS) begin
+            gen.randomize();
+            teste_alternar_modos(gen.num);
+        end
 
         #100 $finish;
     end
