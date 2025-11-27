00a6fa7ea73bd95ffd1fae9f6f240b9119047713
marcovins <marcosbelods@gmail.com>
2025-11-26 22:48:42 -0300

    feat: initial testbench implementation

---
+++ src/fechadura/setup/release_4/tb_setup.sv
@@ -0,0 +1,29 @@
+`timescale 1ns/1ns
+
+
+module testbench_setup;
+    logic clk;
+    logic rst;
+    logic setup_on;
+    senhaPac_t digitos_value;
+    logic digitos_valid;
+    logic display_en;
+    bcdPac_t bcd_pac;
+    setupPac_t data_setup_new;
+    logic data_setup_ok;
+
+    always #1 clk = ~clk;
+
+    setup dut (
+        .clk(clk),
+        .rst(rst),
+        .setup_on(setup_on),
+        .digitos_value(digitos_value),
+        .digitos_valid(digitos_valid),
+        .display_en(display_en),
+        .bcd_pac(bcd_pac),
+        .data_setup_new(data_setup_new),
+        .data_setup_ok(data_setup_ok)
+    );
+
+endmodule
